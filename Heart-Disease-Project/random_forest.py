3bcb3e54b4cceabe2f6383418c4189305252180d
abneripg2025-art <abneripg2025@gmail.com>
2025-12-11 11:49:21 +0000

    Random Forest Classifier

---
+++ Heart-Disease-Project/random_forest.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+from performance_metrics import display_performance_metrics
+
+## Configurations
+train_filename = "./data/cleveland_train_scaled.csv"
+test_filename = "./data/cleveland_test_scaled.csv"
+targets = ["target"]
+
+n_estimators = 115
+max_samples = 15
+
+train_dataset = pd.read_csv(train_filename)
+test_dataset = pd.read_csv(test_filename)
+
+x_train = train_dataset.drop(columns=targets)
+t_train = train_dataset[targets] # real
+
+x_test = test_dataset.drop(columns=targets)
+t_test = test_dataset[targets] # real
+
+model = RandomForestClassifier(
+    n_estimators = n_estimators,
+    max_samples = max_samples
+)
+model.fit(x_train, t_train.squeeze())
+
+y_train = model.predict(x_train) # model output
+y_test = model.predict(x_test) # model output
+
+classes = train_dataset['target'].unique()
+
+display_performance_metrics(t_train, y_train, classes, "Train")
+display_performance_metrics(t_test, y_test, classes, "Test")
